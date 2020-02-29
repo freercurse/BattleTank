65dcb104babff9b49214d6edafa9f2c3d279cb84
freercurse <freerjoseph@yahoo.co.uk>
2020-02-29 01:36:40 +0000

    Inital commit

---
+++ BattleTank/Battleground/Source/Battleground/BattlegroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattlegroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEGROUND_API ABattlegroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
